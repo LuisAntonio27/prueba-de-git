3f1d5ad86b5590d3484ea55f79e867bd6f59a058
Luis Antonio <luisantonio27@live.com.mx>
2020-03-01 22:53:41 -0600

    Se introdujo el titulo en portfolio.blade

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::view('/about', 'about')->name('about');
 Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
 
-Route::resource('projects', 'PortfolioController')->only(['index', 'show']);
+// Route::resource('projects', 'PortfolioController')->only(['index', 'show']);
+
+Route::resource('projects', PortfolioController)->only(['index', 'show']);
