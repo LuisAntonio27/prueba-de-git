69bb75ab5661f498c11b415510aebaee9cecd557
Luis Antonio Cruz <luisantonio27@live.com.mx>
2021-01-13 18:43:56 -0600

    Se actualizan registros

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,17 @@
 |
 */
 
-Route::view('/', 'home')->name('home');
+Route::view('/', 'inicio')->name('inicio');
 Route::view('/about', 'about')->name('about');
+
+Route::put('titan/{cuenta}', function ($cuenta) {
+    return 'Se actualizo la cuenta';
+});
 Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
+Route::post('users/{id}', function ($id) {
+    return 'Cambio en las rutas para post';
+});
 
 // Route::resource('projects', 'PortfolioController')->only(['index', 'show']);
 
