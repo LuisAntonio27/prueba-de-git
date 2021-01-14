6d6cf86e200ec7d568c5f379fef63c1b80298aff
Luis Antonio Cruz <luisantonio27@live.com.mx>
2021-01-13 18:41:22 -0600

    Cambios en la ruta de proyectos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::view('/', 'home')->name('home');
 Route::view('/about', 'about')->name('about');
 Route::get('/portfolio', 'PortfolioController')->name('portfolio');
 Route::view('/contact', 'contact')->name('contact');
+Route::get('/proyectos', function ($id) {
+    return 'Hola desde proyectos';
+});
 
 // Route::resource('projects', 'PortfolioController')->only(['index', 'show']);
 
